ab9e6ed4999bfce89b1f520a318ccbb4bf057f6f
Elias Celada <eliascelada8@gmail.com>
2022-08-27 22:35:49 -0300

    Cambios en el algoritmo

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-alert("Bienvenido a Bamboo restaurantes. A continuación organicemos nuestros clientes por mesas...")
+alert("Bienvenido a Bamboo restaurantes. A continuación organicemos nuestros clientes en las 15 mesas...")
 for (let i = 1; i <= 15; i++) {
-    let ingresarCliente = prompt("Ingrese nombre del cliente: ");
+    let ingresarCliente = prompt("Ingrese nombre del cliente o escriba 0 para salir: ");
+    if (ingresarCliente == 0){
+        break;
+    }
     alert("Mesa N° " + i + " asignada al cliente: " + ingresarCliente);
 }
+alert("¡Gracias por su trabajo! ¡Hasta la próxima!");
