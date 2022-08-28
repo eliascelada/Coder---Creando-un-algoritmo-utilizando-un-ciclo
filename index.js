b0aa97cb002a7cc748156a3303bb584979b65bd3
Elias Celada <eliascelada8@gmail.com>
2022-08-27 22:19:29 -0300

    Aplicando un ciclo

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("Bienvenido a Bamboo restaurantes. A continuación organicemos nuestros clientes por mesas...")
+for (let i = 1; i <= 15; i++) {
+    let ingresarCliente = prompt("Ingrese nombre del cliente: ");
+    alert("Mesa N° " + i + " asignada al cliente: " + ingresarCliente);
+}
